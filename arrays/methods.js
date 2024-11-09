b87c2f0ca189eb66bc0535162d69d1543bbb1a93
RohitDudi <rohitdudijat@yahoo.com>
2024-11-09 18:12:15 +0530

    array methods

---
+++ arrays/methods.js
@@ -0,0 +1,14 @@
+let arr = [2, 4, 5, 7, 8, 9];
+// console.log(arr);
+// console.log(arr.includes(9));
+
+// let newArr = arr.join();
+// console.log(newArr);
+
+let arrSlice = arr.slice(1, 3);
+console.log("array: ", arr);
+console.log("slice array: ", arrSlice);
+
+let arrSplice = arr.splice(1, 3);
+console.log("array: ", arr);
+console.log("splice array: ", arrSlice); 
