f139f1f100df3d4891b5b3567d76671af14eead6
RohitDudi <rohitdudijat@yahoo.com>
2024-11-09 16:52:45 +0530

    date methods in depth

--- basics/date.js
+++ basics/date.js
@@ -1,5 +1,20 @@
+const { log } = require("console");
+
 let currentDate = new Date();
 console.log(currentDate.getDay());
 console.log(currentDate.getDate());
 console.log(currentDate.getFullYear());
+
+// console.log(currentDate);
+// console.log(currentDate.toString());
+// console.log(currentDate.toDateString());
+// console.log(currentDate.toLocaleString());
+
+const customDate = new Date(2001, 0, 20);
+console.log(customDate.toDateString());
+
+const myTimeStamp = Date.now();
+console.log(`time stamp value in MS: `, myTimeStamp);
+console.log(`date value in MS: `, currentDate.getTime());
+console.log(`date value in seconds: `, Math.floor(Date.now() / 1000));
 
